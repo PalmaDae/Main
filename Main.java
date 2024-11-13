88ebfe32bc7cdbd17e27e3197a010314408857df
PalmaDae <arturd277@mail.ru>
2024-11-13 09:42:19 +0300

    Добавлена новая функция

--- Main.java
+++ Main.java
@@ -6,7 +6,10 @@ public class Main{
 	
 	public static void main(String args[]) {
 		
+		Random random = new Random();
+		int numberToGuess = random.nextInt(100) + 1;
 		
+		Scanner scanner = new Scanner(System.in);
 		
 /* 		boolean b = true;
 		
@@ -43,11 +46,6 @@ public class Main{
 		}
 		System.out.println("O_O"); */
 		
-		
-		Random random = new Random();
-		int numberToGuess = random.nextInt(100) + 1;
-		
-		Scanner scanner = new Scanner(System.in);
 		
 /* 		System.out.println("Угадайте число от 1 до 100:");
 		
@@ -113,7 +111,7 @@ public class Main{
 			System.out.println("Чёт ты напутал");
 		} */
 		
-		int sum = 0;
+/* 		int sum = 0;
 		
 		for (int i =1; i<=100; i++){
 			sum+=i;
@@ -136,7 +134,191 @@ public class Main{
 			}else{
 				System.out.println(i + " - Нечётное");
 			}
-		}
+		} */
+		/*
+		int number1 = scanner.nextInt();
+		int number2 = scanner.nextInt();
 		
+		System.out.println(number1+number2); */
+		
+	/* 	double number3 = scanner.nextDouble();
+		double number4 = scanner.nextDouble();
+		
+		String truth = (number3==number4) ? "Yes" : "NO";
+		
+		System.out.println(truth); */
+		
+		/* double numb1 = scanner.nextDouble();
+		double numb2 = scanner.nextDouble();
+		
+		if (numb1<=numb2){
+			numb1=0;
+			System.out.println(numb1 + " " + numb2);
+		} else{
+			System.out.println(numb1 + " " + numb2);
+		} */
+		
+		/* double a,b,c;
+        a=scanner.nextDouble();
+        b=scanner.nextDouble();
+        c=scanner.nextDouble();
+ 
+        if (a>=0) {
+            double n = a * a;
+            System.out.println(n);
+        }
+		if (b>=0) {
+            double y = b * b;
+            System.out.println(y);
+        }
+ 
+        if (c>=0) {
+            double z = c * c;
+            System.out.println(z);
+        } */
+		
+		
+		/* int num = scanner.nextInt();
+		
+		int sum = 0;
+		
+		while (num >0){
+			sum += num%10;
+			num/=10;
+		}
+		System.out.println(sum); */
+		
+		
+		/* double a,b,c;
+        a=scanner.nextDouble();
+        b=scanner.nextDouble();
+        c=scanner.nextDouble();
+		
+		if (a>1 && a<3) {
+            System.out.println(a);
+        }
+		if (b>1 && b<3) {
+            System.out.println(b);
+        }
+ 
+        if (c>1 && c <3) {
+            System.out.println(c);
+        } */
+
+		/* int k = scanner.nextInt();
+		
+		for (int i = 2; i<=9; i++){
+			int ans = i*k;
+			System.out.println(i+"*"+k+"="+ans);	
+		} */
+	/* 	
+ 		double a,b,c,d;
+		a = scanner.nextDouble();
+		b = scanner.nextDouble();
+		c = scanner.nextDouble();
+		d = scanner.nextDouble();
+				
+		
+		if (a<=b && b<=c && c<=d){
+			System.out.println(""+d+" "+d+" "+d+" "+d);
+		} else if (a>b && b>c && c>d){
+			System.out.println(""+a+" "+b+" "+c+" "+d);
+		}else{
+			System.out.println(""+(a*a)+" "+(b*b)+" "+(c*c)+" "+(d*d));
+		} */
+	/* 	
+		double x,y,z;
+		x = scanner.nextDouble();
+		y = scanner.nextDouble();
+		z = scanner.nextDouble();
+		double max_numb_prom = Math.max(x,y);
+		double max_numb = Math.max(z,max_numb_prom);
+		double min_numb_prom = Math.min(x,y);
+		double min_numb = Math.min(min_numb_prom,z);
+		double mid_numb = max_numb - min_numb;
+		if ((x+y)>z){
+			System.out.println("Существует");
+			if ((max_numb*max_numb)< ((mid_numb*mid_numb)+(min_numb*min_numb))){
+				System.out.println("Остроугольный");
+			} else{
+				System.out.println("Другой");
+			}
+		} else{
+			System.out.println("Не существует");
+		}
+		 */
+		
+/* 		double x,y,z;
+		x = scanner.nextDouble();
+		y = scanner.nextDouble();
+		z = scanner.nextDouble();
+		
+		if (x!=z && z!=y){
+			if ((z+x+y)<1){
+				if (z<y && z<x){
+					z= (y+x)/2;
+					System.out.println(""+x+" "+y+" "+z);
+				} else if(y<z && y<x){
+					y = (z+x)/2;
+					System.out.println(""+x+" "+y+" "+z);
+				} else if(x<y && x<z){
+					x = (z+y)/2;
+					System.out.println(""+x+" "+y+" "+z);
+				}
+			} else{
+				if (x<y){
+					x = (y+z)/2;
+					System.out.println(""+x+" "+y+" "+z);
+				} else{
+					y = (x+z)/2;
+					System.out.println(""+x+" "+y+" "+z);
+				}
+			}
+		}else {
+			System.out.println("Числа не попарно различимы");
+		} */
+		
+		
+	/* 	double a,b,c;
+		
+		a = 2;
+		b = 6;
+		c = 2;
+		
+		if (a!= 0){
+			double D = (b*b)-(4*a*c);
+			if (D<0){
+				double D2 = Math.sqrt(D);
+				double x1 = (-b +D2)/2*a;
+				double x2 = (-b -D2)/2*a;
+			}else{
+				System.out.println("Действительных корней нет");
+			}
+		}else{
+			System.out.println("A равен нулю");
+		}
+		 */
+		
+		
+/* 		int x;
+		x = scanner.nextInt();
+		
+		if (x>=0){
+			System.out.println(Integer.toBinaryString(x));
+		}else {
+			System.out.println("Число не подходит");
+		}
+		  */
+		
+		/* int x;
+		x = scanner.nextInt();
+		int i = x;
+		while(i>=x){
+			i =scanner.nextInt();
+		}
+		System.out.println("Число оказалось меньше");
+		 
+		 
+		 */
 	}
 }
